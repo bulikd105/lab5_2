78fae7eee34737ff5a7a512c844392a6e06e0bac
bulikd105 <bulikd105@gmail.com>
2016-07-01 19:53:13 +0200

    Poprawnie klasy Person

--- src/main/java/edu/iis/mto/integrationtest/model/Person.java
+++ src/main/java/edu/iis/mto/integrationtest/model/Person.java
@@ -1,38 +1,62 @@
 package edu.iis.mto.integrationtest.model;
 
-public class Person {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "person")
+public class Person 
+{
+	@Id
+	@Column(unique = true, nullable = false)
 	private Long id;
 
+	@Column(name = "first_name")
 	private String firstName;
 
+	@Column(name = "last_name")
 	private String lastName;
 
-	public Long getId() {
+	public Long getId() 
+	{
 		return id;
 	}
 
-	public void setId(Long id) {
+	public void setId(Long id) 
+	{
 		this.id = id;
 	}
 
-	public String getFirstName() {
+	public String getFirstName() 
+	{
 		return firstName;
 	}
 
-	public void setFirstName(String firstName) {
+	public void setFirstName(String firstName) 
+	{
 		this.firstName = firstName;
 	}
 
-	public String getLastName() {
+	public String getLastName() 
+	{
 		return lastName;
 	}
 
-	public void setLastName(String lastName) {
+	public void setLastName(String lastName) 
+	{
 		this.lastName = lastName;
 	}
 
-	public Person() {
+	@Override
+		public String toString() 
+	{
+			return "Person [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + "]";
 	}
-
+	
+	public Person() 
+	{
+		
+	}
 }
