da92d63ad7a44d7fda9126edb466ce311b2cc9eb
bulikd105 <bulikd105@gmail.com>
2016-07-01 19:56:49 +0200

    Poprawki klasy PersonRepositoryIntegrationTest v2

--- src/test/java/edu/iis/mto/integrationtest/repository/PersonRepositoryIntegrationTest.java
+++ src/test/java/edu/iis/mto/integrationtest/repository/PersonRepositoryIntegrationTest.java
@@ -2,32 +2,38 @@ package edu.iis.mto.integrationtest.repository;
 
 import static edu.iis.mto.integrationtest.repository.PersonBuilder.person;
 import static org.junit.Assert.assertEquals;
+
 import java.util.List;
+
 import org.junit.Test;
+
 import edu.iis.mto.integrationtest.model.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class PersonRepositoryIntegrationTest extends IntegrationTest 
 {
+
 	@Autowired
 	private PersonRepository personRepository;
 
 	@Test
 	public void testCanAccessDbAndGetTestData() 
 	{
-//		List<Person> foundTestPersons = personRepository.findAll();
-//		assertEquals(2, foundTestPersons.size());
+		List<Person> foundTestPersons = personRepository.findAll();
+		assertEquals(2, foundTestPersons.size());
 	}
 
 	@Test
 	public void testSaveNewPersonAndCheckIsPersisted() 
 	{
-//		long count = personRepository.count();
-//		personRepository.save(a(person().withId(count + 1)
-//				.withFirstName("Roberto").withLastName("Mancini")));
-//		assertEquals(count + 1, personRepository.count());
-//		assertEquals("Mancini", personRepository.findOne(count + 1)
-//				.getLastName());
+		long count = personRepository.count();
+		Person person = a(person().withId(count + 1).withFirstName("Roberto").withLastName("Mancini"));
+		
+		personRepository.save(person);
+		assertEquals(count + 1, personRepository.count());
+		assertEquals("Mancini", personRepository.findOne(count + 1).getLastName());
+		
+		personRepository.delete( person );
 	}
 
 	private Person a(PersonBuilder builder) 
