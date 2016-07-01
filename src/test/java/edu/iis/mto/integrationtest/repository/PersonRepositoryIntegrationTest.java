b9d25610f2562b96ea0491ef3f453617950b5459
bulikd105 <bulikd105@gmail.com>
2016-07-01 19:55:37 +0200

    Poprawienie klasy PersonRepositoryIntegrationTest

--- src/test/java/edu/iis/mto/integrationtest/repository/PersonRepositoryIntegrationTest.java
+++ src/test/java/edu/iis/mto/integrationtest/repository/PersonRepositoryIntegrationTest.java
@@ -2,27 +2,26 @@ package edu.iis.mto.integrationtest.repository;
 
 import static edu.iis.mto.integrationtest.repository.PersonBuilder.person;
 import static org.junit.Assert.assertEquals;
-
 import java.util.List;
-
 import org.junit.Test;
-
 import edu.iis.mto.integrationtest.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
 
-
-public class PersonRepositoryIntegrationTest extends IntegrationTest {
-
-	
+public class PersonRepositoryIntegrationTest extends IntegrationTest 
+{
+	@Autowired
 	private PersonRepository personRepository;
 
 	@Test
-	public void testCanAccessDbAndGetTestData() {
+	public void testCanAccessDbAndGetTestData() 
+	{
 //		List<Person> foundTestPersons = personRepository.findAll();
 //		assertEquals(2, foundTestPersons.size());
 	}
 
 	@Test
-	public void testSaveNewPersonAndCheckIsPersisted() {
+	public void testSaveNewPersonAndCheckIsPersisted() 
+	{
 //		long count = personRepository.count();
 //		personRepository.save(a(person().withId(count + 1)
 //				.withFirstName("Roberto").withLastName("Mancini")));
@@ -31,7 +30,8 @@ public class PersonRepositoryIntegrationTest extends IntegrationTest {
 //				.getLastName());
 	}
 
-	private Person a(PersonBuilder builder) {
+	private Person a(PersonBuilder builder) 
+	{
 		return builder.build();
 	}
 
